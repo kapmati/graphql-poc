a063bc5a25c161dfbdcaacdd4dfcfab3faa354e8
kapmati <kapmat77@gmail.com>
2022-05-09 20:57:44 +0200

    Initial commit

---
+++ src/test/java/com/kapmati/graphql/GraphqlPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapmati.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
